896909934a6d8a047896d7388b994aefc9acb0a0
hanlinhe <hanlinhe@igg.com>
2019-10-18 11:17:38 +0800

    异步非静态

---
+++ EasyChatAsync/Client/Client/Program.cs
@@ -0,0 +1,16 @@
+﻿using AsyncClient;
+using System;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+            AsyncTCPClient client = new AsyncTCPClient();
+            client.AsynConnect();
+
+            while (true) ;
+        }
+    }
+}
---
+++ EasyChatAsync/Server/Server/Program.cs
@@ -0,0 +1,17 @@
+﻿using AsyncServer;
+using System;
+
+namespace Server
+{
+    class Program
+    {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+            AsyncTCPServer server = new AsyncTCPServer();
+            server.Start();
+
+            while (true) ;
+
+        }
+    }
+}
